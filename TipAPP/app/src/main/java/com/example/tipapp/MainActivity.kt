fdababbd1d7ad0015f59ee578e7951dab4b24dbb
atrajit-sarkar <atrajit.sarkar@gmail.com>
2024-10-19 20:57:42 +0530

    up

--- TipAPP/app/src/main/java/com/example/tipapp/MainActivity.kt
+++ TipAPP/app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.tipapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tipapp.ui.theme.TipAPPTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +37,65 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TipAPPTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Myapp {
+                TopHeader()
             }
+
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Myapp(content: @Composable () -> Unit) {
+    TipAPPTheme {
+        Scaffold (
+            containerColor = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            content()
+        }
+    }
 }
+
+//@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 134.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(190.dp)
+            .padding(16.dp)
+            .padding(top = 40.dp)// Add padding here
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        shadowElevation = 50.dp,
+        tonalElevation = 20.dp,
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                "Total Per Person",
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Text(
+                "$$total",
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    TipAPPTheme {
-        Greeting("Android")
+    Myapp {
+        TopHeader()
     }
 }
