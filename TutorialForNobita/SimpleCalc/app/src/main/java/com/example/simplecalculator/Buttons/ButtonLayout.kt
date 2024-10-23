ebb39cd7afb251eeebcbb0981063c3b808026276
atrajit-sarkar <atrajit.sarkar@gmail.com>
2024-10-23 15:12:43 +0530

    update

---
+++ TutorialForNobita/SimpleCalc/app/src/main/java/com/example/simplecalculator/Buttons/ButtonLayout.kt
@@ -0,0 +1,11 @@
+package com.example.simplecalculator.Buttons
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Buttons() {
+    Column() {
+
+    }
+}
