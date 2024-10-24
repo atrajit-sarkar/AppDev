2e2e4e01cb5e77d61d6cb4acbcda0d2cf4874102
atrajit-sarkar <atrajit.sarkar@gmail.com>
2024-10-24 23:34:57 +0530

    up

--- MovieApp/app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
@@ -1,2 +1,76 @@
-package com.example.movieapp.screens.home.details
+package com.example.movieapp.screens.details
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+//@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsScreen(navController: NavController, movieData: String?) {
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Row(horizontalArrangement = Arrangement.Start) {
+                    Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Arrow Back",
+                        modifier = Modifier.clickable {
+                            navController.popBackStack()
+                        })
+                    Spacer(modifier = Modifier.width(100.dp))
+                    Text(text="Movie")
+
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.Transparent
+            ),
+            modifier = Modifier.shadow(5.dp), // Apply shadow modifier for elevation effect
+            navigationIcon = {
+
+            }
+        )
+    }) { innerPadding ->
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.padding(innerPadding)) {
+
+                Text(
+                    text = movieData.toString(),
+                    style = MaterialTheme.typography.headlineMedium
+                )
+
+            }
+
+        }
+    }
+}
 
--- MovieApp/app/src/main/java/com/example/movieapp/screens/home/HomeScreen.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/screens/home/HomeScreen.kt
@@ -1,2 +1,74 @@
 package com.example.movieapp.screens.home
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.movieapp.MovieRow
+import com.example.movieapp.navigation.MovieScreens
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Movies") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFFf6b58d)
+                ),
+                modifier = Modifier.shadow(5.dp) // Apply shadow modifier for elevation effect
+            )
+        }
+    ) { innerPadding ->
+
+        MainContent(navController, innerPadding)
+
+    }
+
+}
+
+@Composable
+fun MainContent(
+    navController: NavController,
+    innerPadding: PaddingValues,
+    movieList: List<String> = listOf(
+
+        "Avatar",
+        "Fifty Shades Of Grey",
+        "Chichore",
+        "Harry Poter",
+        "3 Idiots",
+        "Abar Proloy",
+        "HouseFull",
+        "Mirzapur"
+    )
+) {
+    Column(modifier = Modifier.padding(innerPadding)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it) { movie ->
+                    navController.navigate(route = MovieScreens.DetailsScreen.name+"/$movie")
+//                    Log.d("Movie", "MainContent: $movie")
+
+                }
+
+            }
+        }
+    }
+
+
+}
